4d347e13488f0b4e64231ab3f0c07966fae3c512
Edwix01 <edwinlima201@gmail.com>
2025-08-13 00:02:51 -0500

    Práctica hasta el capítulo 6

--- estado_real/__manifest__.py
+++ estado_real/__manifest__.py
@@ -3,5 +3,11 @@
     "description": "Módulo para gestionar propiedades inmobiliarias",
     "author": "Edwin Lima",
     "depends":[ "base"],
+    "data": [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estado_real_menu.xml',
+    ],
     'application': True,
+
 }
